a799bac896733b826aca46bc96018e219176a605
Sabaniki <sawada13@mccc.jp>
2020-04-16 23:56:33 +0900

    当たり判定があり、当たるとくるくる回ってるキューブが消える。スコアを表示する部分を製作中

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
-    private Rigidbody rb;
     public float speed;
+    public Text countText;
+
+    private Rigidbody rb;
+    private int count;
+
+    private void UpdateCountText() => countText.text = "Count: " + count.ToString();
 
     private void Start() {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        UpdateCountText();
     }
 
     // 物理演算が実行される直前に呼び出されるメソッド。物理演算に関するコードをこのメソッドに記述する。
@@ -17,6 +25,12 @@ public class PlayerController : MonoBehaviour {
         var moveVertical = Input.GetAxis("Vertical");
         var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement * speed);
-        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!other.gameObject.CompareTag("PickUp")) return;
+        other.gameObject.SetActive(false);
+        count++;
+        UpdateCountText();
     }
 }
