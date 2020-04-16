456dec1f67e9254243b4e8df4f7960028fba07d3
Sabaniki <sawada13@mccc.jp>
2020-04-16 16:52:16 +0900

    とりあえずボールを動かせ、キューブがくるくるその場で回るようになった。当たり判定はまだない。

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset;
+    
     // Start is called before the first frame update
-    void Start() {
+    private void Start() {
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
-    void Update() {
+    private void LateUpdate() {
+        transform.position = player.transform.position + offset;
     }
 }
